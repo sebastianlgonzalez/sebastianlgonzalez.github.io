bc687a209971a1f07199af9052023051c13a7205
sebastianlgonzalez <sebastianlgonzalez01@gmail.com>
2023-10-21 17:53:40 -0400

    static webpage generation

--- pages/projects/[project].js
+++ pages/projects/[project].js
@@ -1,20 +1,39 @@
 import { useRouter } from 'next/router'
-import Error from 'next/error'
 import projects from "@/components/Projects/projects.config.json"
 import Redirection from '@/components/Projects/Redirection.js'
 import styles from '@/styles/project.module.css'
+
+export const getStaticProps = async () => {
+  return {
+    props: {
+      projects,
+    }
+  };
+}
+
+export const getStaticPaths = async () => {
+  const paths = projects
+    .filter((project) => project.type == 0)
+    .map((project) => ({
+      params: {
+        project: project.id,
+      },
+    }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+
 export default function Project() {
   const router = useRouter()
   const currentProject = projects.find(project => project.id == router.query.project && project.type == 0);
 
-  if (currentProject) {
-    return (
-      <div id={styles.wrapper}>
-        <Redirection url={currentProject.href} />
-      </div>
-      
-    );
-  } else {
-    return <Error statusCode={404} />
-  }
+  return (
+    <div id={styles.wrapper}>
+      <Redirection url={currentProject.href} />
+    </div>
+  )
 }
